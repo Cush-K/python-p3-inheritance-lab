3cccae245b175937c9e54c2771b89082207b9024
Kalvine <kkalvine4@gmail.com>
2024-09-02 15:16:50 +0300

    Applies super()

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,26 @@
 from user import User
 
 class Student(User):
+    def __init__(self, firstname, lastname, knowledge = None):
+        super().__init__(firstname, lastname)
+        if knowledge is None:
+            knowledge = [
+                "str is a data type in Python",
+                "programming is hard, but it's worth it",
+                "JavaScript async web request",
+                "Python function call definition",
+                "object-oriented teacher instance",
+                "programming computers hacking learning terminal",
+                "pipenv install pipenv shell",
+                "pytest -x flag to fail fast",
+            ]
+            
+        self.knowledge = knowledge
     
-    def learn(self):
-        pass
+    def learn(self, new_info):
+        self.knowledge.append(new_info)
+        return self.knowledge
+    
+
+boy = Student("John","Doe")
+print(boy.learn("stuffs!"))
--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,25 @@ from user import User
 import random
 
 class Teacher(User):
-
+    def __init__(self,firstname, lastname, knowledge=None):
+        super().__init__(firstname, lastname)
+        if knowledge is None:
+            knowledge = [
+                "str is a data type in Python",
+                "programming is hard, but it's worth it",
+                "JavaScript async web request",
+                "Python function call definition",
+                "object-oriented teacher instance",
+                "programming computers hacking learning terminal",
+                "pipenv install pipenv shell",
+                "pytest -x flag to fail fast",
+            ]
+        self.knowledge = knowledge
+    
+    
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
+    
+    
+teacher = Teacher("John", "Doe")
+print(teacher.teach())
